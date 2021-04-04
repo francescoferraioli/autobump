d05eff72e3ee092e5005eb04f0d7ee6f7df43da2
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-04-04 16:30:25 +1000

    Dry up the build scripts

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -4,17 +4,5 @@ set -e
 set -o nounset
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-REPO="francescoferraioli/autobump-action"
 
-pushd "${SCRIPT_DIR}/.." > /dev/null
-
-set -x
-
-docker build -t "${REPO}:v1" .
-docker tag "${REPO}:v1" "${REPO}:latest"
-docker push "${REPO}:v1"
-docker push "${REPO}:latest"
-
-set +x
-
-popd > /dev/null
+sh "${SCRIPT_DIR}/build.sh" "stable"
